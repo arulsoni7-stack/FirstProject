3704c41a44d1d14683aed0e691f2f66a089a0bfa
Arul Kumar <arulsoni7@gmail.com>
2026-05-16 20:53:33 +0530

    second commit

--- src/Arul.java
+++ src/Arul.java
@@ -1,5 +1,9 @@
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Arul {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        System.out.println("Jaaaa");
     }
 }
